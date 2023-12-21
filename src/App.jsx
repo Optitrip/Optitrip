6e1382de41e6e55aac6e40b81466abfe34ca9a81
Juan <you@example.com>
2023-12-21 16:16:33 -0600

    abrir y cerrar

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,7 @@ export default function App(props) {
         .then(response => response.json())
         .then(data=>{
             var destinations=state.destinations;
-            destinations[index].name=data["items"][0]["address"]["label"];
+            destinations[index].name='data["items"][0]["address"]["label"]';
             setState(prevState => ({ ...prevState,
                 destinations: destinations
             }));
@@ -138,18 +138,11 @@ export default function App(props) {
         )
 }
 
-function DestinationsBoxes(props){
-    return(
-        <div>
-        
-        </div>
-    )
-}
 function SearchComponent(props) {
     const [search, setSearch]=useState({
         values:[],
         query:"",
-        selected_element:null,
+        selected_lat_lng:null,
         reply_places:[],
     })
     function updateResponse(event) {
@@ -159,13 +152,21 @@ function SearchComponent(props) {
         })
     }
     function addToDestinations(map){
-        var lat=search.selected_element.target.dataset.lat;
-        var lng=search.selected_element.target.dataset.lng;
+        var lat=search.selected_lat_lng.lat;
+        var lng=search.selected_lat_lng.lng;
         props.addToDestinations(map, lat, lng);
         setSearch({
             ...search,
             reply_places:[]
         })
+    }
+    function showInMap() {
+        console.log(search.selected_lat_lng);
+        moveMapToPlace(map,search.selected_lat_lng.lat, search.selected_lat_lng.lng);
+        setSearch({
+            ...search,
+            reply_places:[]
+        });
     }
     function searchApi(){
         var fetch_link="";
@@ -174,25 +175,31 @@ function SearchComponent(props) {
         .then(response => response.json())
         .then(data=>{
             let places=[];
+            let onchange_radio=[]
+            let n=0
             data["items"].forEach(place=>{
-                places.push(<PlaceSearchButton setSearch={setSearch} lat={place.position.lng} lng={place.position.lng} place={place}/>);
+                onchange_radio.push(<PlaceSearchButton key={`place-${n}`} radio_name="places_result" state={search} setSearch={setSearch} lat={place.position.lat} lng={place.position.lng} place={place}/>);
+                n++;
             })
             places.push(
             <>
-            <div className="row gx-5 mt-2">
+            <div>
+                {onchange_radio}
+            </div>
+            <div class="row gx-5 mt-2">
                 <div className="col d-flex justify-content-center">
-                <button className="btn btn-secondary"><i className="fa fa-arrow-left"></i></button>
+                <button class="btn btn-secondary"><i class="fa fa-arrow-left"></i></button>
                 </div>
                 <div className="col d-flex justify-content-center">
-                <button className="btn btn-primary"><i className="fa fa-arrow-right"></i></button>
+                <button class="btn btn-primary"><i class="fa fa-arrow-right"></i></button>
                 </div>
             </div>
-            <div className="row gx-5 mt-2">
+            <div class="row gx-5 mt-2">
                 <div className="col d-flex justify-content-center">
-                <button className="btn btn-warning" onClick={()=>moveMapToPlace(search.selected_element.dataset.lat, search.selected_element.dataset.lng)}>Mostrar en el mapa</button>
+                <button class="btn btn-warning" onClick={showInMap}>Mostrar en el mapa</button>
                 </div>
                 <div className="col d-flex justify-content-center">
-                <button className="btn btn-primary" onClick={()=>addToDestinations(map)}>Agregar parada</button>
+                <button class="btn btn-primary" onClick={()=>addToDestinations(map)}>Agregar parada</button>
                 </div>
             </div>
             </>
@@ -209,7 +216,7 @@ function SearchComponent(props) {
             <input id="query" list="datalist_results" onKeyUp={updateResponse} type="search" placeholder={props.placeholder} style={{display: 'inline-block', maxWidth:"50vw"}} className="form-control mr-sm-2"/>
             <button onClick={searchApi} type="button" style={{display:'inline-block', marginLeft: '-20px'}} className="btn btn-primary"><i className="fas fa-map-marker-alt"></i></button>
             <button onClick={searchApi} type="button" style={{display:'inline-block', marginLeft: '0px'}} className="btn btn-primary"><i className="fas fa-search"></i></button>
-        <div className="shift_started_modal" style={{display:search.reply_places.length<=0?"none":"block"}}>
+        <div class="shift_started_modal" style={{display:search.reply_places.length<=0?"none":"block"}}>
             <div className="modal-dialog" role="document">
                 <div className="modal-content p-3 rounded-4 shadow">
                     <div className="modal-header border-bottom-0">
@@ -229,17 +236,19 @@ function SearchComponent(props) {
 }
 
 function PlaceSearchButton(props) {
-    const [state, setState]=useState({
-        selected:false
-    })
-    const lat=props.lat;
-    const lng=props.lng;
-    function placeSelected() {
-        setState(prevState => ({ ...prevState, selected:true}));
-        props.setSearch(prevState => ({ ...prevState, selected_element:[lat,lng]}))
+    let color="btn-secondary";
+    if(props.state.selected_lat_lng){
+        color=props.state.selected_lat_lng.lat==parseFloat(props.lat)&&props.state.selected_lat_lng.lng==parseFloat(props.lng)?"btn-primary":"btn-secondary"
+    }
+    function onChangeRadio() {
+        console.log({"lat":parseFloat(props.lat),"lng":parseFloat(props.lng)});
+        props.setSearch(prevState => ({ ...prevState, selected_lat_lng:{"lat":parseFloat(props.lat),"lng":parseFloat(props.lng)}}))
     }
     return(
-        <button className={`btn btn-block ${state.selected?"btn-primary":"btn-secondary"}`} data-lat={props.lat} data-lng={props.lng} onClick={()=>placeSelected()}>{props.place.address.label}</button>
+        <label className={`btn btn-block ${color}`}>
+            {props.place.address.label}
+            <input type="radio" style={{display:"none"}} onChange={onChangeRadio} value={[props.lat,props.lng]} name={props.radio_name}></input>
+        </label>
     )
 }
 
@@ -287,8 +296,8 @@ function DestinationsModal(props) {
             destinations.push(
             <div style={{display:"inline-block"}}>
                 <i className={`fa fas-marker ${marker_color}}`}></i>
-                <button className='btn btn-light'><i className="fa-solid fa-grip text-secondary"></i></button>
-                <button className="btn btn-block btn-secondary">{props.state.destinations[index].name}</button>
+                <button className='btn btn-light'><i class="fa-solid fa-grip text-secondary"></i></button>
+                <button class="btn btn-block btn-secondary">{props.state.destinations[index].name}</button>
             </div>
             );
         }
@@ -296,13 +305,13 @@ function DestinationsModal(props) {
     }
     return (
         <div>
-            <div className="form-group">
+            <div class="form-group">
             <SearchComponent userPosition={props.state.current_position} addToDestinations={props.addToDestinations} placeholder="Seleccionar destino"/>
             <div className="container border bg-light p-3 rounded shadow-lg m-2">
                 {destinations()}
             </div>
             </div>
-            <button type="button" className="btn-block btn-primary btn" onClick={()=>openModal(false)}>Listo!</button>
+            <button type="button" class="btn-block btn-primary btn" onClick={()=>openModal(false)}>Listo!</button>
         </div>
     )
 }
@@ -313,10 +322,10 @@ function TransportationModal(props) {
     }
     return(
             <a>
-                <input type="radio" onChange={updateTransportation} className="transportation" value="car" name="transportation" id=""/><i className="fas fa-car transportation-icon"></i>
-                <input type="radio" onChange={updateTransportation} className="transportation" value="truck" name="transportation" id=""/><i className="fas fa-truck transportation-icon"></i>
-                <input type="radio" onChange={updateTransportation} className="transportation" value="pedestrian" name="transportation" id=""/><i className="fas fa-walking transportation-icon"></i>
-                <input type="radio" onChange={updateTransportation} className="transportation" value="bicycle" name="transportation" id=""/><i className="fas fa-bicycle transportation-icon"></i>
+                <input type="radio" onChange={updateTransportation} class="transportation" value="car" name="transportation" id=""/><i class="fas fa-car transportation-icon"></i>
+                <input type="radio" onChange={updateTransportation} class="transportation" value="truck" name="transportation" id=""/><i class="fas fa-truck transportation-icon"></i>
+                <input type="radio" onChange={updateTransportation} class="transportation" value="pedestrian" name="transportation" id=""/><i class="fas fa-walking transportation-icon"></i>
+                <input type="radio" onChange={updateTransportation} class="transportation" value="bicycle" name="transportation" id=""/><i class="fas fa-bicycle transportation-icon"></i>
             </a>
     )
 }
@@ -340,11 +349,11 @@ function TimeModal(props) {
 function ModeModal(props) {
     return(
         <a>
-            <span className="icon"><i className="fas fa-tachometer-alt"></i></span>
+            <span class="icon"><i class="fas fa-tachometer-alt"></i></span>
             <h4>Forma de viaje:</h4>
-            <input type="radio" checked className="mode" value="none" name="mode" id=""/><span>Ninguno</span>
-            <input type="radio" className="mode" value="fastest" name="mode" id=""/><span>Rápida</span>
-            <input type="radio" className="mode" value="shortest" name="mode" id=""/><span>Corta</span>
+            <input type="radio" checked class="mode" value="none" name="mode" id=""/><span>Ninguno</span>
+            <input type="radio" class="mode" value="fastest" name="mode" id=""/><span>Rápida</span>
+            <input type="radio" class="mode" value="shortest" name="mode" id=""/><span>Corta</span>
         </a>
     )
 }
@@ -353,47 +362,47 @@ function AvoidModal(params) {
     return (
         <a>
             <div>
-                <input type="checkbox" className="avoid" value="seasonalClosure" name="avoid" id="">
+                <input type="checkbox" class="avoid" value="seasonalClosure" name="avoid" id="">
                     <span>Clausura estacional</span>
                 </input>
             </div>
             <div>
-                <input type="checkbox" className="avoid" value="controlledAccessHighway" name="avoid" id="">
+                <input type="checkbox" class="avoid" value="controlledAccessHighway" name="avoid" id="">
                     <span>Autopista de acceso controlado</span>
                 </input>
             </div>
             <div>
-                <input type="checkbox" className="avoid" value="carShuttleTrain" name="avoid" id="">
+                <input type="checkbox" class="avoid" value="carShuttleTrain" name="avoid" id="">
                     <span>Cruce con tren</span>
                 </input>
             </div>
             <div>
-                <input type="checkbox" className="avoid" value="dirtRoad" name="avoid" id="">
+                <input type="checkbox" class="avoid" value="dirtRoad" name="avoid" id="">
                     <span>Camino de tierra</span>
                 </input>
             </div>
             <div>
-                <input type="checkbox" className="avoid" value="uTurns" name="avoid" id="">
+                <input type="checkbox" class="avoid" value="uTurns" name="avoid" id="">
                     <span>Vuelta en U</span>
                 </input>
             </div>
             <div>
-                <input type="checkbox" className="avoid" value="difficultTurns" name="avoid" id="">
+                <input type="checkbox" class="avoid" value="difficultTurns" name="avoid" id="">
                     <span>Carretera de cobro</span>
                 </input>
             </div>
             <div>
-                <input type="checkbox" name="avoid" value="ferry"  className="avoid" id="">
+                <input type="checkbox" name="avoid" value="ferry"  class="avoid" id="">
                     <span>Ferry</span>
                 </input>
             </div>
             <div>
-                <input type="checkbox" name="avoid" value="tunnel" className="avoid" id="">
+                <input type="checkbox" name="avoid" value="tunnel" class="avoid" id="">
                     <span>Túnel</span>
                 </input>
             </div>
             <div>
-                <input type="checkbox" name="avoid" value="tollRoad" className="avoid" id="">
+                <input type="checkbox" name="avoid" value="tollRoad" class="avoid" id="">
                     <span>Giros complicados</span>
                 </input>
             </div>
